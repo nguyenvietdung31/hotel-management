08be20e015ef4b1977c2364c4678809a24c7c116
fu_nmt02 <88494626+FUnmthank02@users.noreply.github.com>
2022-12-14 21:20:15 +0700

    Update Layout 3

--- src/Component/Rooms/Detail.js
+++ src/Component/Rooms/Detail.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from '../Header_Footer/Header'
 import Footer from '../Header_Footer/Footer'
 import slide_img1 from '../../Image/slide_img_1.jpg'
@@ -6,18 +6,25 @@ import slide_img2 from '../../Image/slide_img_2.jpg'
 import slide_img3 from '../../Image/slide_img_3.jpg'
 import slide_img4 from '../../Image/slide_img_4.jpg'
 import './Detail.scss'
-import { DatePicker, Space, Select } from 'antd';
+import AOS from 'aos'
+import { DatePicker, Space } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
-
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+dayjs.extend(customParseFormat)
 const { RangePicker } = DatePicker;
 
 function Detail() {
-    dayjs.extend(customParseFormat);
 
     // const [listDate, setListDate] = useState(['2022-12-14', '2022-12-15','2022-12-20']);
-    // array chua list date can disable (vd: "2020-09-20")
+    
+    // set time for aos animation
+    useEffect(() => {
+        AOS.init({ duration: 1000 })
+    }, [])
 
+    /* To disable date that booked */
     const disabledDate = (current) => {
         const arr = [
             {
@@ -51,7 +58,7 @@ function Detail() {
             <div className="container wrap_detail">
                 <div className="row">
                     <div className="col-lg-8 col-sm-12 col-xs-12">
-                        <div id="myCarousel" className="carousel slide contain_slider mt-5" data-ride="carousel">
+                        <div data-aos="fade-right" id="myCarousel" className="carousel slide contain_slider mt-5" data-ride="carousel">
 
                             {/* Slider */}
                             <div className="carousel-inner">
@@ -80,23 +87,43 @@ function Detail() {
 
                         <div className="row">
                             <div className="about_room col-lg-12 col-sm-12 col-xs-12">
-                                <div className="title">
+                                <div className="title" data-aos="fade-left">
                                     <p className='room_name font-weight-bold'>Luxury Room 1</p>
                                     <p className='room_price font-weight-bold'>$250 / Night</p>
                                 </div>
                                 <div className="row">
-                                    <div className="description col-lg-8 col-sm-12 col-xs-12">
-                                        <p className='room_description'>This is a luxury room with many grate service, beautiful view. It will make you relaxable.</p>
+                                    <div data-aos="fade-right" className="description col-lg-8 col-sm-6 col-xs-12">
+                                        <table class="table table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th>Type</th>
+                                                    <th>Bed</th>
+                                                    <th>Size</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>Standard</td>
+                                                    <td>1 double bed</td>
+                                                    <td>20 m²</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+
+                                        <div data-aos="fade-right" className="item_description mt-4">
+                                            <p className='item_description_title font-weight-bold'>Description</p>
+                                            <p className='room_description'>This is a luxury room with many grate service, beautiful view. It will make you relaxable.</p>
+                                        </div>
                                     </div>
-                                    <div className="description col-lg-4 col-sm-12 col-xs-12">
-                                        <div className="room_services">
+                                    <div className="wrap_room_services col-lg-4 col-sm-6 col-xs-12">
+                                        <div data-aos="fade-right" className="room_services">
                                             <p className="room_services_title font-weight-bold">Room Services</p>
                                             <ul>
-                                                <li>Private bathroom</li>
-                                                <li>Free Wifi</li>
-                                                <li>Free Lunch</li>
-                                                <li>Bottled Mineral Water</li>
-                                                <li>Hot/Cold Shower & Bathtub</li>
+                                                <li><FontAwesomeIcon icon={faCircleCheck} className='mr-2 text-success' />Private bathroom</li>
+                                                <li><FontAwesomeIcon icon={faCircleCheck} className='mr-2 text-success' />Free Wifi</li>
+                                                <li><FontAwesomeIcon icon={faCircleCheck} className='mr-2 text-success' />Free Lunch</li>
+                                                <li><FontAwesomeIcon icon={faCircleCheck} className='mr-2 text-success' />Bottled Mineral Water</li>
+                                                <li><FontAwesomeIcon icon={faCircleCheck} className='mr-2 text-success' />Hot/Cold Shower & Bathtub</li>
                                             </ul>
                                         </div>
                                     </div>
@@ -105,8 +132,8 @@ function Detail() {
                         </div>
                     </div>
 
-                    <div className="col-lg-4 col-sm-12 col-xs-12">
-                        <div className="wrap_reservation mt-5">
+                    <div className="col-lg-4 col-sm-8 col-xs-12">
+                        <div data-aos="fade-up" className="wrap_reservation mt-5">
                             <div className="heading">
                                 <p className='mb-0 font-weight-bold'>Your Reservation</p>
                             </div>
@@ -116,8 +143,8 @@ function Detail() {
                                     <p className='mb-2 font-weight-bold'>Date</p>
                                 </div>
                                 <div className='wrap_rangepicker'>
-                                    <Space direction="vertical" size={12}>
-                                        <RangePicker
+                                    <Space direction="vertical" size={12} style={{width: '100%'}}> 
+                                        <RangePicker style={{width: '100%'}}
                                             size='large'
                                             placeholder={['Check In', 'Check Out']}
                                             disabledDate={disabledDate}
@@ -127,7 +154,7 @@ function Detail() {
                                 </div>
                             </div>
                             <div className="wrap_button">
-                                <button className='btn btn-warning'>Book now!</button>
+                                <a href='/reservation' className='btn btn-success'>Book now!</a>
                             </div>
 
 
